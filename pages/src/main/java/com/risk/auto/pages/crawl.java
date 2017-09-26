bf9b3d2544352a1eb3d717be8f8d545b9e8a2e88
zhengjiali <zhengjiali@zhengjldeMacBook-Air.local>
2017-09-26 16:43:13 +0800

    add dealexcel class

--- pages/src/main/java/com/risk/auto/pages/crawl.java
+++ pages/src/main/java/com/risk/auto/pages/crawl.java
@@ -1,5 +1,6 @@
 package com.risk.auto.pages;
 
+import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
@@ -11,29 +12,39 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.*;
 import org.testng.*;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 
 import org.testng.Assert;
 import java.io.File;
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 
 
 
 public class crawl {
 	
 	WebDriver driver;
-	String base_url="http://risk.bat.tcredit.com/risk/trics/login";
+	String uri="http://risk.bat.tcredit.com";
+	Date date = new Date();
+	DateFormat format = new SimpleDateFormat("yyyyMMddHHmmss");
+	String time = format.format(date);
+	String path = "../pic/"+time;
+	File file;
 	
-	@BeforeTest
+	
+	@BeforeClass
 	public void setUp() throws IOException{
-		
+		file = new File(path);
+		if( !file.exists() && !file.isDirectory()){
+			file.mkdir();
+		}
 		System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
 		driver = new ChromeDriver();
-		driver.manage().window().maximize();
-		driver.get(base_url);
+		driver.get(uri+"/risk/trics/login");
 		driver.manage().timeouts().implicitlyWait(5,TimeUnit.SECONDS);
 		login();
 	}
@@ -49,8 +60,9 @@ public class crawl {
 		new WebDriverWait(driver, 15 ).until(
 				ExpectedConditions.presenceOfElementLocated(By.cssSelector("body > div.container > div.clearfix.header > a"))
 		);
+		driver.manage().window().maximize();
 		File screenshot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-		FileUtils.copyFile(screenshot, new File("/Users/tcxy/Documents/workspace/auto_page/pages/src/pic/planrun.jpg"));
+		FileUtils.copyFile(screenshot, new File(path+"/planrun.jpg"));
 //		Assert.assertEquals(driver.getTitle(), "方案执行");
 		
 	}
@@ -58,27 +70,106 @@ public class crawl {
 	@Test
 	public void plan() throws IOException{
 		try{
-			driver.get("http://risk.bat.tcredit.com/risk/trics/plan/toQuery");
+			driver.get(uri+"/risk/trics/plan/toQuery");
+			driver.manage().window().maximize();
 			Assert.assertEquals(driver.getTitle(), "方案配置");
 		}finally{
 			File screenshot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-			FileUtils.copyFile(screenshot, new File("/Users/tcxy/Documents/workspace/auto_page/pages/src/pic/plan.jpg"));
+			FileUtils.copyFile(screenshot, new File(path+"/plan.jpg"));
+		}
+	}
+	
+	@Test
+	public void rule() throws IOException{
+		try{
+			driver.get(uri+"/risk/trics/rule/toQuery");
+			driver.manage().window().maximize();
+			Assert.assertEquals(driver.getTitle(), "规则管理");
+		}finally{
+			File screenshot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+			FileUtils.copyFile(screenshot, new File(path+"/rule.jpg"));
 		}
 	}
 	
 	@Test
 	public void ruleGroup() throws IOException{
 		try{
-			driver.get("http://risk.bat.tcredit.com/risk/trics/ruleGroup/toQuery");
+			driver.get(uri+"/risk/trics/ruleGroup/toQuery");
+			driver.manage().window().maximize();
 			Assert.assertEquals(driver.getTitle(), "规则集管理");
 		}finally{
 			File screenshot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-			FileUtils.copyFile(screenshot, new File("/Users/tcxy/Documents/workspace/auto_page/pages/src/pic/ruleGroup.jpg"));
+			FileUtils.copyFile(screenshot, new File(path+"/ruleGroup.jpg"));
+		}
+	}
+	
+	@Test
+	public void model() throws IOException{
+		try{
+			driver.get(uri+"/risk/trics/model/toQuery");
+			driver.manage().window().maximize();
+			Assert.assertEquals(driver.getTitle(), "模型管理");
+		}finally{
+			File screenshot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+			FileUtils.copyFile(screenshot, new File(path+"/model.jpg"));
+		}
+	}
+	
+	@Test
+	public void rulePackage() throws IOException{
+		try{
+			driver.get(uri+"/risk/trics/rulePackage/toElementPackagePage?packageType=1");
+			driver.manage().window().maximize();
+			Assert.assertEquals(driver.getTitle(), "R包函数管理");
+		}finally{
+			new WebDriverWait(driver,5).until(
+					ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id='list']/table/tbody/tr[1]/td[2]"))
+			);
+			File screenshot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+			FileUtils.copyFile(screenshot, new File(path+"/rulePackage.jpg"));
 		}
 	}
 	
 	
-	@AfterTest
+	@Test
+	public void definedVariable() throws IOException{
+		try{
+			driver.get(uri+"/risk/trics/definedVariable/toDataDefinePage");
+			driver.manage().window().maximize();
+			Assert.assertEquals(driver.getTitle(), "自定义数据管理");
+		}finally{
+			File screenshot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+			FileUtils.copyFile(screenshot, new File(path+"/definedVariable.jpg"));
+		}
+	}
+	
+	@Test
+	public void undefinedVariable() throws IOException{
+		try{
+			driver.get(uri+"/risk/trics/undefinedVariable/toDataUndefinePage");
+			driver.manage().window().maximize();
+			Assert.assertEquals(driver.getTitle(), "非自定义数据管理");
+		}finally{
+			File screenshot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+			FileUtils.copyFile(screenshot, new File(path+"/undefinedVariable.jpg"));
+		}
+	}
+	
+	@Test
+	public void paramVariable() throws IOException{
+		try{
+			driver.get(uri+"/risk/trics/paramVariable/toDataParamPage");
+			driver.manage().window().maximize();
+			Assert.assertEquals(driver.getTitle(), "数据调取参数管理");
+		}finally{
+			File screenshot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+			FileUtils.copyFile(screenshot, new File(path+"/paramVariable.jpg"));
+		}
+	}
+	
+	
+	
+	@AfterClass
 	public void tearDown(){
 		driver.close();
 	}
---
+++ pages/src/main/java/com/risk/auto/pages/dealexcel.java
@@ -0,0 +1,143 @@
+package com.risk.auto.pages;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.concurrent.TimeUnit;
+import java.util.ArrayList;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.*;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.*;
+import org.testng.*;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+
+import org.testng.Assert;
+import java.io.File;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
+public class dealexcel {
+	WebDriver driver;
+	String uri="http://risk.bat.tcredit.com";
+	Date date = new Date();
+	DateFormat format = new SimpleDateFormat("yyyyMMddHHmmss");
+	String time = format.format(date);
+	String root_path = "../pic/"+time;
+	String currPath;
+	File file;
+	ArrayList<HashMap<String, String>> pages;
+	
+	
+	@BeforeClass
+	public void setUp() throws IOException{
+		System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
+		driver = new ChromeDriver();
+		driver.get(uri+"/risk/trics/login");
+		driver.manage().timeouts().implicitlyWait(5,TimeUnit.SECONDS);
+		new_dir(root_path);
+	}
+	
+	@Parameters({"admin1"})
+	@BeforeMethod
+	public void login(String admin1) throws IOException{
+		WebElement ElementName = driver.findElement(By.id("user_name"));
+		WebElement ElementPwd = driver.findElement(By.id("pwd"));
+		ElementName.sendKeys("admin1");
+		ElementPwd.sendKeys(admin1);
+		WebElement ElementLogin = driver.findElement(By.className("submit_btn"));
+		ElementLogin.click();
+		System.out.println(driver.getTitle());
+		new WebDriverWait(driver, 15 ).until(
+				ExpectedConditions.presenceOfElementLocated(By.cssSelector("body > div.container > div.clearfix.header > a"))
+		);
+		currPath = root_path+"/"+"admin1";
+		new_dir(currPath);
+	}
+	
+	
+	@Test
+	public void getPages() throws InvalidFormatException, IOException{
+		InputStream inp = new FileInputStream("../pages.xlsx");
+		Workbook wb = WorkbookFactory.create(inp);
+	    Sheet pages = wb.getSheetAt(1);
+	    int lastRowNum = pages.getLastRowNum();
+	    for(int i = 1;i<=lastRowNum;i++){
+	    	Row row = pages.getRow(i);
+	    	try{
+	    		getPage(uri+row.getCell(2).getStringCellValue(),currPath,row.getCell(1).getStringCellValue(),row.getCell(3).getStringCellValue());
+	    	}catch(Error e){
+	    		row.createCell(4).setCellValue("Fail");
+	    		continue;
+	    	}
+	    	row.createCell(4).setCellValue("Pass");
+	    }
+	    FileOutputStream out = null;
+	    try{
+	    	out = new FileOutputStream("../pages.xlsx");
+	    	wb.write(out);
+	    }catch(IOException e){
+	    	e.printStackTrace();
+	    }finally{
+	    	try{
+	    		out.close();
+	    	}catch(IOException e){
+	    		e.printStackTrace();
+	    	}
+	    }
+	}
+	
+	@AfterClass
+	public void tearDown(){
+		driver.close();
+	}
+	
+	public void new_dir(String dir){
+		File file = new File(dir);
+		if( !file.exists() && !file.isDirectory()){
+			file.mkdir();
+		}
+	}
+	
+	public void getPage(String url,String dir,String kw,String title){
+		try{
+			driver.get(url);
+			driver.manage().window().maximize();
+			Assert.assertEquals(driver.getTitle(), title);
+		}finally{
+			File screenshot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+			try {
+				FileUtils.copyFile(screenshot, new File(dir+"/"+kw+".jpg"));
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+			}
+		}
+	}
+		
+
+}
